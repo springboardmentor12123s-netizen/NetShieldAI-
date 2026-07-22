9caa728b65ad2bf6de058192ca0ef78a315c8d3d
Aryan <arynkr07@gmail.com>
2026-07-22 13:44:11 +0530

    backend changess

--- backend/database/postgres.py
+++ backend/database/postgres.py
@@ -3,8 +3,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from dotenv import load_dotenv
+import os
 # Make sure your actual password is in this URL
-SQLALCHEMY_DATABASE_URL = "POSTGRES_URL"
+load_dotenv
+
+# 2. Use os.getenv() to fetch the value assigned to POSTGRES_URL in your .env file
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "POSTGRES_URL", 
+    "postgresql://postgres:fallback_password@localhost:5432/netshield_users"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
