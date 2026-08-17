4a4ed2742dabfdfba52857750fb0fad89f121594
Lasyapriya putti <24b01a1286@gmail.com>
2026-08-17 20:39:02 +0530

    Add Docker deployment configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,8 +23,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8080",
-        "http://localhost:5173",
+    "http://localhost:4173",
+    "http://localhost:8080",
+    "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
